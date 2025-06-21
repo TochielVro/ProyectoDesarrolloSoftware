fb64d0410efce906acf334b239efddeffb416b9c
TochielVro <heidan.toribio@gmail.com>
2025-06-21 12:11:42 -0500

    Conexxion a la base de datos

--- Backend/Database/connection.js
+++ Backend/Database/connection.js
@@ -1,16 +1,8 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: 'TochielVroXd12',
-database: 'bd_ventas'
-});
-connection.connect((err) => {
-    if (err) {
-console.error('Error de conexión:', err);
-return;
-}
-console.log('Conectado a MySQL');
+  host: 'localhost',
+  user: 'root',
+  password: 'TochielVroXd12',
+  database: 'bd_localink'
 });
 module.exports = connection;
-// This code establishes a connection to a MySQL database using the mysql2 library.
