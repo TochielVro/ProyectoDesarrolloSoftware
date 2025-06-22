58d512c221317b6019fe1812f42e7dc1c6dc7ef9
TochielVro <heidan.toribio@gmail.com>
2025-06-22 11:39:03 -0500

    Middlewares para el manejo de la auntenticacion y la subida de imagenes

---
+++ Backend/Middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const verificarToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  
+  if (!token) {
+    return res.status(403).json({ error: 'Token no proporcionado' });
+  }
+
+  jwt.verify(token, 'secreto', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Token inválido' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+const esAdmin = (req, res, next) => {
+  if (!req.user?.esAdmin) {
+    return res.status(403).json({ error: 'Acceso denegado' });
+  }
+  next();
+};
+
+module.exports = { verificarToken, esAdmin };
