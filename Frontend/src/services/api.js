87a9a4f66692aae02b9a370df9ea7d39a34e3336
Jairo221451 <221451@unsaac.edu.pe>
2025-06-25 23:22:24 -0500

    Implementacion del api.js

---
+++ Frontend/src/services/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3001/api';
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Interceptor para añadir el token a las peticiones
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export const registerUser = (userData) => api.post('/auth/register', userData);
+export const loginUser = (credentials) => api.post('/auth/login', credentials);
+export const getEmprendimientos = () => api.get('/emprendimientos');
+
+export default api;
