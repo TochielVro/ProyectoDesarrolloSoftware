50f59ad9af66ba3d1182c8e0af593dcd5b1cb71a
Jairo221451 <221451@unsaac.edu.pe>
2025-06-26 11:26:06 -0500

    Mejora del MainLayout.jsx

--- Frontend/src/Layouts/MainLayout.jsx
+++ Frontend/src/Layouts/MainLayout.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
+import Sidebar from '../Components/Sidebar';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const MainLayout = () => {
   return (
     <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <main className="flex-grow-1">
-        <Outlet />
-      </main>
+      <Container fluid className="flex-grow-1">
+        <Row className="h-100">
+          {/* Sidebar - ocupa 3 columnas en pantallas grandes */}
+          <Col lg={3} className="d-none d-lg-block p-0">
+            <Sidebar />
+          </Col>
+          
+          {/* Contenido principal - ocupa 9 columnas en pantallas grandes */}
+          <Col lg={9} className="py-4">
+            <Outlet />
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </div>
   );
